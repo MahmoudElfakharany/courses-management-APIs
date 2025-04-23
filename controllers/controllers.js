6a3790ab39277d311ce55aef30efb36f71a83702
Mahmoud El-fakharany <m.a.el-fakharany@hotmail.com>
2025-04-23 22:49:36 +0200

    FEAT: Applying multer for uploading

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -3,9 +3,10 @@ const course = require('../models/course');
 exports.getAllCourses = async (req, res) => {
   try {
     const courses = await course.find();
+    if(!courses.length) return res.status(404).json({ message: 'No courses added' });
     res.status(200).json(courses);
   } catch (err) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: 'Server error', error:err });
   }
 };
 
@@ -15,13 +16,12 @@ exports.getCourseById = async (req, res) => {
     if (!course) return res.status(404).json({ message: 'Course not found' });
     res.status(200).json(course);
   } catch (err) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: 'Server error',error:err  });
   }
 };
 
 exports.createCourse = async (req, res) => {
-  const { title, description, image, startDate, endDate, price } = req.body;
-
+  const { title, description, startDate, endDate, price } = req.body;
   if (!title || !description || !price)
     return res.status(400).json({ message: 'Title, description, and price are required' });
 
@@ -32,7 +32,7 @@ exports.createCourse = async (req, res) => {
     await newCourse.save();
     res.status(201).json({ message: 'Course created', course: newCourse });
   } catch (err) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: 'Server error',error:err });
   }
 };
 
@@ -40,12 +40,12 @@ exports.updateCourse = async (req, res) => {
   try {
     const updates = { ...req.body };
     if (req.file) updates.image = req.file.path;
-    
+
     const course = await course.findByIdAndUpdate(req.params.id, req.body, { new: true });
     if (!course) return res.status(404).json({ message: 'Course not found' });
     res.status(200).json({ message: 'Course updated', course });
   } catch (err) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: 'Server error',error:err  });
   }
 };
 
@@ -55,6 +55,6 @@ exports.deleteCourse = async (req, res) => {
     if (!course) return res.status(404).json({ message: 'Course not found' });
     res.status(200).json({ message: 'Course deleted' });
   } catch (err) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: 'Server error',error:err  });
   }
 };
